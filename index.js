dec009b83d3a2341b23808e2b17e28d2efd66360
abunaim34 <farhanadnanfarabi@gmail.com>
2024-12-08 20:58:42 +0600

    server setup

--- index.js
+++ index.js
@@ -24,10 +24,6 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
-        // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
-
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
         const database = client.db('gamerDB');
         const reviewCollection = client.db('gamerDB').collection('reviews');
@@ -114,6 +110,11 @@ async function run() {
             const result = await reviewCollection.deleteOne(query);
             res.send(result);
         })
+        
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     } finally {
         // Ensures that the client will close when you finish/error
